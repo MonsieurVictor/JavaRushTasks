01125764ba5f613699ff9e18a6be00e8ca86b82f
MonsieurVictor <34025717+MonsieurVictor@users.noreply.github.com>
2020-02-22 21:35:02 +0200

    lvl 5
    lvl 5

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+
+
+    }
+
+    Solution(short s){}
+    private Solution(int i){}
+    public Solution(){}
+    protected Solution(double d){}
+
+
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+    public SubSolution() {
+    }
+    SubSolution(short s){}
+
+    protected SubSolution(double d){}
+
+
+}
